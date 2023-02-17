99f33622bae6ee6833cbc97a4614a4809c0ec4e4
Roshan <roshan.fallah.621@my.csun.edu>
2023-02-16 16:36:58 -0800

    pushing week 4 homework tidyr

---
+++ Week_04/Scripts/lab_tidyr_homework_for_grading.R
@@ -0,0 +1,35 @@
+### This is my lab homework week 4 Thursday ####
+### Created by: Roshan Fallah #############
+### Updated on: 2023-02-16 ####################
+
+library(tidyverse)  ## loading libraries ##
+library(here)
+
+ChemData<-read_csv(here("Week_04","Data", "chemicaldata_maunalua.csv"))
+View(ChemData)
+glimpse(ChemData)
+
+tidyr<- ChemData %>% 
+  separate(col = Tide_time, # choose the tide time col
+         into = c("Tide","Time"), #Separating into two columns for Tide and time
+         sep = "_")%>% # separate by _
+  filter(complete.cases(.)) %>%
+  filter(Season == "SPRING") %>%
+  select(Season, Tide, Time, Salinity, pH) %>%
+  pivot_longer(cols = Salinity:pH, # the cols you want to pivot. This says select the temp to percent SGD cols  
+                               names_to = "Variables", # the names of the new cols with all the column names 
+                               values_to = "Values") %>%
+  group_by(Variables, Season, Time) %>%
+  summarise(mean_vals = mean(Values, na.rm = TRUE)) ## calculating mean  and removing NA's for the calc values to not mess up ##
+
+ggplot(data = tidyr,
+       mapping = aes(x = Time,
+                     y = mean_vals,
+                     group = Variables,
+                     color = Variables))+
+  geom_point()+
+  labs(x = "Time",
+       y = "Salinity pH")
+       
+          
+  
---
+++ Week_04/Scripts/week4b_tidyr.R
@@ -0,0 +1,64 @@
+### Today we are going to practice tidy with biogeochemistry data from Hawaii ####
+### Created by: Roshan Fallah #############
+### Updated on: 2023-02-16 ####################
+
+
+## loading libraries###
+
+library(tidyverse)
+library(here)
+### Load data ######
+ChemData<-read_csv(here("Week_04","Data", "chemicaldata_maunalua.csv"))
+View(ChemData)
+glimpse(ChemData)
+
+ChemData_clean<-ChemData %>%
+filter(complete.cases(.))%>%
+separate(col = Tide_time, # choose the tide time col
+         into = c("Tide","Time"), # separate it into two columns Tide and time
+         sep = "_", # separate by _
+  remove = FALSE) %>%
+    unite(col = "Site_Zone", # the name of the NEW col
+          c(Site,Zone), # the columns to unite
+          sep = ".", # lets put a . in the middle
+          remove = FALSE)
+  
+  ChemData_long <- ChemData_clean%>%
+  pivot_longer(cols = Temp_in:percent_sgd, # the cols you want to pivot. This says select the temp to percent SGD cols
+               names_to = "Variables", # the names of the new cols with all the column names
+               values_to = "Values")
+  
+
+chemData_long %>%
+  group_by(Variables, Site) %>% # group by everything we want 
+  summarise(Param_means = mean(Values, na.rm = TRUE), # get mean 
+            Param_vars = var(Values, na.rm = TRUE),
+            Param_stdv# get variance  
+
+
+ChemData_long %>%
+  ggplot(aes(x = Site, y = Values))+
+  geom_boxplot()+
+  facet_wrap(~Variables)
+
+ChemData_wide<-ChemData_long %>%
+  pivot_wider(names_from = Variables, # column with the names for the new columns
+              values_from = Values)
+
+
+ChemData_clean<-ChemData %>%
+  drop_na() %>% #filters out everything that is not a complete row
+  separate(col = Tide_time, # choose the tide time col
+           into = c("Tide","Time"), # separate it into two columns Tide and time
+           sep = "_", # separate by _
+           remove = FALSE) %>%
+  pivot_longer(cols = Temp_in:percent_sgd, # the cols you want to pivot. This says select the temp to percent SGD cols  
+               names_to = "Variables", # the names of the new cols with all the column names 
+               values_to = "Values") %>% # names of the new column with all the values 
+  group_by(Variables, Site, Time) %>%
+  summarise(mean_vals = mean(Values, na.rm = TRUE)) %>%
+pivot_wider(names_from = Variables, 
+            values_from = mean_vals) %>% # notice it is now mean_vals as the col name
+  write_csv(here("Week_04","Output","summary.csv"))  # export as a csv to the right folder
+
+View(ChemData_clean)
